3d1d5f7e5179bb4da22018eb3d76bc173761f4a1
KM <tsnok777@gmail.com>
2022-06-30 12:59:32 +0300

    scrapping verbose name

---
+++ src/scrapping/__init__.py
@@ -0,0 +1,2 @@
+
+default_app_config ='scrapping.apps.ScrappingConfig'
--- src/scrapping/apps.py
+++ src/scrapping/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ScrappingConfig(AppConfig):
     name = 'scrapping'
+    verbose_name = 'job collection application'
