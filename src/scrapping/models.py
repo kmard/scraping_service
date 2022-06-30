b29836243ab68eb895f208eead0671b4548b43b6
KM <tsnok777@gmail.com>
2022-06-30 13:24:50 +0300

    scrapping unique filds

--- src/scrapping/models.py
+++ src/scrapping/models.py
@@ -8,8 +8,8 @@ class City(models.Model):
     #python manage.py makemigrations
     #python manage.py migrate
     name = models.CharField(max_length=50,
-                            verbose_name='name city')
-    slug = models.CharField(max_length=50, blank=True)
+                            verbose_name='name city',unique=True)
+    slug = models.CharField(max_length=50, blank=True,unique=True)
 
     class Meta:
         verbose_name = 'city name'
