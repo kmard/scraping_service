90541394be20c9e9a46b6e74eb9d5728d86d0eb4
KM <tsnok777@gmail.com>
2022-07-13 15:57:32 +0300

    synchronous scrapping

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,9 +1,5 @@
-# import asyncio
-# import codecs
-import codecs
+import asyncio #multiprosessing/multithreading
 import os, sys
-# import datetime as dt
-#
 from django.contrib.auth import get_user_model
 from django.db import DatabaseError
 
@@ -25,6 +21,7 @@ parsers = (
     (djinni, 'djinni'),
     (rabota, 'rabota')
 )
+jobs, errors = [], []
 
 def get_settings():
     qs = User.objects.filter(send_email=True).values()
@@ -50,66 +47,30 @@ settings = get_settings()
 url_list = get_urls(settings)
 
 
+async def main(value):
+    func, url,city,language = value
+    job,err = await loop.run_in_executor(None,func,url,city,language)
+    errors.extend(err)
+    jobs.extend(job)
 
-# city = City.objects.filter(slug='kiev').first()
-# language = language.objects.filter(slug='piton').first()
+loop = asyncio.get_event_loop()
+tmp_tasks = [(func, data['url_data'][key], data['city'], data['language'])
+             for data in url_list
+             for func, key in parsers]
 
-jobs, errors = [], []
 
-for data in url_list:
+if tmp_tasks:
+    tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
+    loop.run_until_complete(tasks)
+    loop.close()
 
-    for func,key  in parsers:
-        url = data['url_data'][key]
-        j, e = func(url,city = data['city'],language = data['language'])
-        jobs+=j
-        errors+=e
 
 for job in jobs:
     v = Vacancy(**job)
     try:
         v.save()
     except DatabaseError:
-        continue
-
-if errors:
-    er = Error(data = errors).save()
-
-
-
-#
-#
-# async def main(value):
-#     func, url, city, language = value
-#     job, err = await loop.run_in_executor(None, func, url, city, language)
-#     errors.extend(err)
-#     jobs.extend(job)
-#
-# settings = get_settings()
-# url_list = get_urls(settings)
-#
-# loop = asyncio.get_event_loop()
-# tmp_tasks = [(func, data['url_data'][key], data['city'], data['language'])
-#              for data in url_list
-#              for func, key in parsers]
-#
-# # for data in url_list:
-# #
-# #     for func, key in parsers:
-# #         url = data['url_data'][key]
-# #         j, e = func(url, city=data['city'], language=data['language'])
-# #         jobs += j
-# #         errors += e
-# if tmp_tasks:
-#     tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
-#     loop.run_until_complete(tasks)
-#     loop.close()
-#
-# for job in jobs:
-#     v = Vacancy(**job)
-#     try:
-#         v.save()
-#     except DatabaseError:
-#         pass
+        pass
 # if errors:
 #     qs = Error.objects.filter(timestamp=dt.date.today())
 #     if qs.exists():
@@ -118,9 +79,5 @@ if errors:
 #         err.save()
 #     else:
 #         er = Error(data=f'errors:{errors}').save()
-#
-# # h = codecs.open('work.txt', 'w', 'utf-8')
-# # h.write(str(jobs))
-# # h.close()
-# ten_days_ago = dt.date.today() - dt.timedelta(10)
-# Vacancy.objects.filter(timestamp__lte=ten_days_ago).delete()
+
+
